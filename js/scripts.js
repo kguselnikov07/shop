e92ebd73f27f2fd1fc49a6f68b39c5c05c15dff6
Kos <example@gmail.com>
2023-03-28 15:15:22 +0500

    адаптив index tablet

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@ let button = document.getElementById('burger');
 
 let navigation = document.getElementById('menu');
 
+let menuClose = document.getElementById('close')
+
 button.addEventListener('click', function(){
     navigation.classList.toggle('display-block');
 });
+
+menuClose.addEventListener('click', function(){
+    navigation.classList.toggle('display-block');
+});
+
